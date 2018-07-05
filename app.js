658e613d71a587006544cc46dd6b864bb1da76e0
khirodhar54 <40377565+khirodhar54@users.noreply.github.com>
2018-07-05 15:31:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let app=angular.module("myApp", []);
+
+app.controller("myCtr", function($scope, $http){
+    $http.get("data.json").then(function(item){
+        $scope.players=item.data;
+    })
+});
